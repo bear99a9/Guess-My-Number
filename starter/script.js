a38e5c1d8230d6f7130b9e06d6770985bd08426c
Sean Edwards <you@example.com>
2021-02-18 12:40:32 +0000

    added secretNumber withh Math.random()

--- starter/script.js
+++ starter/script.js
@@ -9,6 +9,8 @@
 
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
